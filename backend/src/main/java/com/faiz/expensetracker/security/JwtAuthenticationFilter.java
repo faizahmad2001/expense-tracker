2e52939e2236e0dbb1d77a340779678bae67538a
Faiz Ahmad <faizahmad8292@gmail.com>
2026-09-20 17:32:31 +0530

    Fix JWT authentication filter

--- backend/src/main/java/com/faiz/expensetracker/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/faiz/expensetracker/security/JwtAuthenticationFilter.java
@@ -1,15 +1,19 @@
 package com.faiz.expensetracker.security;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
+import java.io.IOException;
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
